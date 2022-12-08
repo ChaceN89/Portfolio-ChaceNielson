5a8800263b91928360555b69668897cb6949e5a8
Chace Nielson <chacen@shaw.ca>
2022-12-07 21:54:12 -0700

    set up pop out for gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,22 +1,44 @@
 /**
  * Page containing a gallery of all photos
  */
-import React from 'react'
+import React, { useState } from 'react'
 import {carouselData} from '../carouselData'
 import SectionHeader from '../components/SectionHeader'
 
 function Gallery() {
   // randomize the data
-  const images = carouselData.sort(() => Math.random() - 0.5)
+  // const images = carouselData.sort(() => Math.random() - 0.5)
+
+    // for model pop up
+  const [model, setModel] = useState(false)
+  const [tempImage, setTempImage] = useState("")
+
+
+  const getImg =(e)=>{
+    setTempImage(e)
+    setModel(true)
+  }
+  const ccc =()=>{
+    setModel(false)
+  }
 
   return (
     <section className='p-5'>
       <SectionHeader title= {"My Gallery"} description={"Some of my photos"}/>
+
+      <div className={model ? "model isOpen" : "modal"} onClick={ccc} >
+        <img id='image'  src={tempImage} alt="...model" className='  rounded-xl '/>
+        
+      </div>
+
+
+
+
       <div className='gallery p-4 md:p-6'>
-        {images.map((item, index)=>{
+        {carouselData.map((item, index)=>{
           return(
             <div className='pics'>
-              <img id='image' key={index} src={item.image} alt="..." className=' object-contain rounded-xl py-2 hover:opacity-90'/>
+              <img id='image' onClick={()=>getImg(item.image)} key={index} src={item.image} alt="..." className=' object-contain rounded-xl py-2 hover:opacity-60'/>
             </div>
           )
         })
