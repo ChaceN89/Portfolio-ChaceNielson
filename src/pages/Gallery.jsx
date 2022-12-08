a599ba256a9db50f1c2a34b362e4bf3ac37c4eb9
Chace Nielson <chacen@shaw.ca>
2022-12-07 21:11:18 -0700

    Set up the gallery ppage

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,14 +1,28 @@
 /**
- * Page containing a gallery of photos acces
+ * Page containing a gallery of all photos
  */
 import React from 'react'
+import {carouselData} from '../carouselData'
+import SectionHeader from '../components/SectionHeader'
 
 function Gallery() {
+  // randomize the data
+  const images = carouselData.sort(() => Math.random() - 0.5)
+
   return (
-    <div className='h-screen items-center flex flex-col space-y-5 justify-center font-bold'> 
-    <div className='text-3xl'>Photo Gallery</div>
-    <div className='text-2xl'>Coming Soon!</div>
-  </div>
+    <section className='p-5'>
+      <SectionHeader title= {"My Gallery"} description={"Some of my photos"}/>
+      <div className='gallery p-4 md:p-6'>
+        {images.map((item, index)=>{
+          return(
+            <div className='pics'>
+              <img id='image' key={index} src={item.image} alt="..." className=' object-contain rounded-xl py-2 hover:opacity-90'/>
+            </div>
+          )
+        })
+      }
+      </div>
+    </section>
   )
 }
 
