9137cc7e15d771c5e8787cd53a3e605917ac9ff8
Chace Nielson <chacen@shaw.ca>
2023-09-25 19:09:33 -0600

    rerounded the footer picture

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
   return (
     <footer className=" text-lg bg-white bg-opacity-10 backdrop-blur-xl shadow-lg h-fit py-6">
       <div className="container mx-auto flex flex-col items-center space-y-4 md:space-y-0 md:flex-row md:space-x-5 md:justify-around  ">
-        <img className="w-fit sm:h-36 rounded-3xl object-top my-1" src={process.env.PUBLIC_URL + '/photos/portraits/portrait1.jpg'} alt="..."/>
+        <img className="w-fit sm:h-36 rounded-full object-top my-1" src={process.env.PUBLIC_URL + '/photos/portraits/portrait1.jpg'} alt="..."/>
         <Link className='cursor-pointer text-4xl font-bold' duration={500} to={"Home"} spy smooth > Chace Nielson </Link>
         <ExternalLinks size={25}/>
       </div>
