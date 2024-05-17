3bf90968376a90ac2e80228534f01814f96e6e98
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-05-16 22:17:32 -0600

    added new functionality to open the pdfs in a separate window before download

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -3,13 +3,13 @@
  * Uses download icon for all instances
  */
 import React from 'react'
-import {AiOutlineCloudDownload} from 'react-icons/ai'
+import { AiOutlineDownload } from "react-icons/ai";
 
 function DownloadButton({onClick, name}) {
     return (
         <button onClick={onClick} className='relative group rounded-xl cursor-pointer flex justify-between items-center overflow-hidden  hover:bg-white hover:text-black bg-gray-700 bg-opacity-20 shadow-2xl backdrop-blur-3xl'>
             <div className=' p-2 bg-white bg-opacity-20   flex justify-center items-center '>
-                <AiOutlineCloudDownload size={30} className="hover:text-black hover:scale-105"/>
+                <AiOutlineDownload size={30} className="hover:text-black hover:scale-105"/>
             </div>
             <div className="px-4">{name}</div> 
         </button>
--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -3,31 +3,17 @@
  */
 import React from 'react'
 import DownloadButton from './DownloadButton';
+import openPdf from '../functions/openPDF';
 
 function DownloadCV() {
-    const downloadResume = () => {
-		try { 
-			const fileName = "Chace Nielson - Resume.pdf" 
-			var FileSaver = require('file-saver');
-			FileSaver.saveAs(process.env.PUBLIC_URL + '/pdfs/'+fileName, fileName);
-		} catch {
-			return
-		}
-    }
-    const downloadCoverLetter = () => {
-		try { 
-			const fileName = "Chace Nielson - Cover Letter.pdf"
-			var FileSaver = require('file-saver');
-			FileSaver.saveAs(process.env.PUBLIC_URL + '/pdfs/'+fileName, fileName);
-		} catch {
-			return
-		}
-    }
+	// functions to open pdfs in a separate
+	const openResume = () => openPdf("Chace Nielson - Resume.pdf");
+    const openCoverLetter = () => openPdf("Chace Nielson - Cover Letter.pdf");
 
 	return (
 		<div className='py-6 flex justify-center space-x-4 sm:space-x-20 lg:space-x-36'>
-			<DownloadButton onClick={downloadResume} name={"Resume"}/>
-			<DownloadButton onClick={downloadCoverLetter} name={"Cover Letter"}/>
+			<DownloadButton onClick={openResume} name={"Resume"}/>
+			<DownloadButton onClick={openCoverLetter} name={"Cover Letter"}/>
 		</div>
 	)
 }
