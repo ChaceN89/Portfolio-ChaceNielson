b689070698effae5219a0459c273cac5c156c317
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2025-01-27 14:37:31 -0700

    added cube game

--- src/data/projects/cubeGame.jsx
+++ src/data/projects/cubeGame.jsx
@@ -14,18 +14,26 @@
 import IconLibrary from "../iconLibrary";
 
 // Short description (blurb) to be displayed on the project card
-const short = "An endless runner game developed using Unity Game Engine.";
+const short = "A simple yet addictive endless runner built built.";
 
 // Long description for the modal page, providing detailed information about the project
 const long = (
-  <div className='space-y-2'>
+  <div className="space-y-2">
     <p>
-      This project is an endless runner game created using Unity and C#. Players control a cube, navigating it through a series of challenging levels and obstacles.
+      Cube Game is a fast-paced endless runner developed in Unity, where players guide a cube through challenging obstacles in an ever-changing environment. The goal is to survive as long as possible while avoiding traps and barriers.
     </p>
     <p>
-      The game demonstrates essential game mechanics and physics, highlighting the capabilities of Unity for developing engaging and interactive experiences.
+      This project showcases the fundamentals of game development, including dynamic physics, responsive controls, and level design. It's a simple yet fun experience that demonstrates the power of Unity for creating interactive games.
     </p>
-    <iframe className="rounded-xl"  frameBorder="0" src="https://itch.io/embed/2918493?border_width=5&amp;link_color=FFA500" width="100%" height="165"><a href="https://kar2fast.itch.io/cube-game">Cube Game by Kar2fast</a></iframe>
+    <iframe
+      className="rounded-xl"
+      frameBorder="0"
+      src="https://itch.io/embed/2918493?border_width=5&amp;link_color=FFA500"
+      width="100%"
+      height="165"
+    >
+      <a href="https://kar2fast.itch.io/cube-game">Cube Game by Kar2fast</a>
+    </iframe>
   </div>
 );
 
@@ -35,13 +43,14 @@ const features = [
 
 export const cubeGame = {
   id: "cube-game",
-  name: "Endless Runner Game",
+  name: "Cube game",
   tag: "Game",
   blurb: short,
   description: long,
   features: features,
   images: [
-    { src: "cubegame.jpg", blurhash: "LHO:^eD%4.%M00xv%2of_Nxt-;D%" },
+    { src: "cube00.jpg", blurhash: "LHO:^eD%4.%M00xv%2of_Nxt-;D%" },
+    { src: "cube0.jpg", blurhash: "LHO:^eD%4.%M00xv%2of_Nxt-;D%" },
     { src: "cube1.jpg", blurhash: "LHO:^eD%4.%M00xv%2of_Nxt-;D%" },
     { src: "cube2.jpg", blurhash: "LHO:^eD%4.%M00xv%2of_Nxt-;D%" },
     { src: "cube3.jpg", blurhash: "LHO:^eD%4.%M00xv%2of_Nxt-;D%" },
