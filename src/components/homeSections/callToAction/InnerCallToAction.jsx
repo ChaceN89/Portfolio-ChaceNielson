f556e393e0328ca28479e2d9f7227e2bd680bce7
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2025-01-28 22:13:45 -0700

    updated call to action section

--- src/components/homeSections/callToAction/InnerCallToAction.jsx
+++ src/components/homeSections/callToAction/InnerCallToAction.jsx
@@ -6,10 +6,8 @@
  * 
  * @author Chace Nielson
  * @created 2024-07-28
- * @updated 2024-07-28
- * @since 2.1
+ * @updated Jan 25 2025
  */
-
 import React from 'react';
 import { callToActionData } from '../../../data/pageData/callToActionData';
 // Components
@@ -18,19 +16,22 @@ import ImageComponent from '../../wrappers/ImageComponent';
 
 function InnerCallToAction() {
   return (
-    <div className="inner-call-to-action bg-frosted-glass">
-      <div className="col-span-2 hidden md:flex items-end justify-end">
+    <div className="inner-call-to-action backdrop-blur-sm">
+      {/* Image Positioned on the Left, Touching Bottom */}
+      <div className="image-container">
         <ImageComponent
           src={callToActionData.img}
           lowResSrc={callToActionData.lowResImg}
           alt="Chace Hiking"
-          className="object-contain max-w-40 h-56 md:max-w-56 md:h-72" // Adjust width and height as needed
+          className="object-contain h-full w-full"
         />
       </div>
-      <div className="col-span-1 flex flex-col h-full justify-center items-start p-6">
-        <h2>{callToActionData.top}</h2>
-        <h4 className="mt-2">{callToActionData.bottom}</h4>
-        <div className="mt-6 flex justify-start md:justify-start space-x-2 z-30">
+
+      {/* Text and Button Section */}
+      <div className="flex flex-col h-full justify-center items-start p-6 w-full md:w-3/4 lg:w-3/4 self-center">
+        <h2 className="text-white font-bold">{callToActionData.top}</h2>
+        <h4 className="mt-2 text-white">{callToActionData.bottom}</h4>
+        <div className="mt-6 flex justify-start space-x-2">
           {callToActionData.buttons.map((button) => (
             <MyButton key={button.id} to={button.id}>
               {button.name}
@@ -43,3 +44,4 @@ function InnerCallToAction() {
 }
 
 export default InnerCallToAction;
+
